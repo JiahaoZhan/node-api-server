43cfc1e4b7b6d367f20f0d95c1acb065521cc0fc
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-28 02:57:07 -0400

    Update dbConfig.js

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('heroku_f7469a0c6b39995', 'b53afc7fb548c0', '47dd204f', 
+const sequelize = new Sequelize('heroku_8b0e70c41dd5b65', 'b53afc7fb548c0', '47dd204f', 
 { 
 dialect: 'mysql', 
 host: "us-cdbr-east-06.cleardb.net"
 });
-
+mysql://b53afc7fb548c0:47dd204f@us-cdbr-east-06.cleardb.net/heroku_8b0e70c41dd5b65?reconnect=true
 module.exports = sequelize
