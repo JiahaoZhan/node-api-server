c600586e19c6fea0a6421dd08f56d7886a68995b
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-28 04:10:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const path = require('path')
 app.use(bodyParser.json()); // 解析json数据格式
 app.use(bodyParser.urlencoded({ extended: true })); // 解析form表单提交的数据application/x-www-form-urlencoded
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use("/", router);
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile('index.html')
 })
 
 
