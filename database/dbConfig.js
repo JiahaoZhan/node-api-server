22681896b5a2db909427af86223f8cf4d055d0f0
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-28 15:52:02 -0400

    favicon

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -5,5 +5,10 @@ const sequelize = new Sequelize('heroku_8b0e70c41dd5b65', 'b53afc7fb548c0', '47d
 dialect: 'mysql', 
 host: "us-cdbr-east-06.cleardb.net"
 });
+// const sequelize = new Sequelize('nodeapidb', 'root', 'root', 
+// { 
+// dialect: 'mysql', 
+// host: "127.0.0.1"
+// });
 mysql://b53afc7fb548c0:47dd204f@us-cdbr-east-06.cleardb.net/heroku_8b0e70c41dd5b65?reconnect=true
 module.exports = sequelize
