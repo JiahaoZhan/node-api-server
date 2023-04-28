f538c44f214c51b64c18311fcea200bc5b12c97d
JiahaoZhan <Ichirojp201@gmail.com>
2023-04-28 04:42:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const sequelize = require("./database/dbConfig");
 const app = express();
 const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json()); // 解析json数据格式
 app.use(bodyParser.urlencoded({ extended: true })); // 解析form表单提交的数据application/x-www-form-urlencoded
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/", router);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 sequelize.sync().then((result) => {
   app.listen(process.env.PORT || 8088, () => {
